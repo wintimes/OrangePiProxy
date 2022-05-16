371b04cb3aaa05b604dfebb2e56f4f65c27e9268
Keyboardkiller <wintimes@users.noreply.github.com>
2022-05-16 08:21:22 +0800

    Update

--- OrangepiProxy.sh
+++ OrangepiProxy.sh
@@ -19,28 +19,28 @@ if [[ $option_i0 == "y" ]]; then
 
         echo -e "Enter LAN proxy ip:port such as 192.168.31.31:8118"
         read IP
-        echo -e "IP=${IP}" >> '/home/keyboardkiller/ip_proxy'
+        echo -e "IP=${IP}" >> '/etc/ip_proxy'
         #get ip_proxy from file ip
-        ip_proxy=`sed '/^IP=/!d;s/.*=//' /home/keyboardkiller/ip_proxy`
+        ip_proxy=`sed '/^IP=/!d;s/.*=//' /etc/ip_proxy`
 
         echo -e "Do you want change profile proxy? [y/n]"
         read option_i2
         if [[ $option_i2 == "y" ]]; then
             #proxy for profile
-            chmod 777 '/home/keyboardkiller/profile'
-            echo -e "export proxy='http://${ip_proxy}'\nexport all_proxy=\$proxy" >> '/home/keyboardkiller/profile' 
-            chmod 644 '/home/keyboardkiller/profile'
+            chmod 777 '/etc/profile'
+            echo -e "export proxy='http://${ip_proxy}'\nexport all_proxy=\$proxy" >> '/etc/profile' 
+            chmod 644 '/etc/profile'
         fi
         echo -e "Do you want change wget proxy? [y/n]"
         read option_i3
         if [[ $option_i3 == "y" ]]; then
             #proxy for wget
-            chmod 777 '/home/keyboardkiller/wgetrc'
-            sed -i "s@#https_proxy = http://proxy.yoyodyne.com:18023/@https_proxy = http://${ip_proxy}/@g" "/home/keyboardkiller/wgetrc"
-            sed -i "s@#http_proxy = http://proxy.yoyodyne.com:18023/@http_proxy = http://${ip_proxy}/@g" "/home/keyboardkiller/wgetrc"
-            sed -i "s@#ftp_proxy = http://proxy.yoyodyne.com:18023/@ftp_proxy = http://${ip_proxy}/@g" "/home/keyboardkiller/wgetrc"
-            sed -i "s@#use_proxy = on@use_proxy = on@g" "/home/keyboardkiller/wgetrc"
-            chmod 644 '/home/keyboardkiller/wgetrc'
+            chmod 777 '/etc/wgetrc'
+            sed -i "s@#https_proxy = http://proxy.yoyodyne.com:18023/@https_proxy = http://${ip_proxy}/@g" "/etc/wgetrc"
+            sed -i "s@#http_proxy = http://proxy.yoyodyne.com:18023/@http_proxy = http://${ip_proxy}/@g" "/etc/wgetrc"
+            sed -i "s@#ftp_proxy = http://proxy.yoyodyne.com:18023/@ftp_proxy = http://${ip_proxy}/@g" "/etc/wgetrc"
+            sed -i "s@#use_proxy = on@use_proxy = on@g" "/etc/wgetrc"
+            chmod 644 '/etc/wgetrc'
         fi
     fi
     
@@ -52,29 +52,29 @@ if [[ $option_i0 == "y" ]]; then
         echo -e "Enter LAN proxy ip:port such as 192.168.31.31:8118"
         read ip_proxy_change        
         #get last ip_proxy from file-ip_proxy
-        ip_proxy=`sed '/^IP=/!d;s/.*=//' /home/keyboardkiller/ip_proxy`
+        ip_proxy=`sed '/^IP=/!d;s/.*=//' /etc/ip_proxy`
         
-        sed -i "s@IP=${ip_proxy}@IP=${ip_proxy_change}@g" "/home/keyboardkiller/ip_proxy"
+        sed -i "s@IP=${ip_proxy}@IP=${ip_proxy_change}@g" "/etc/ip_proxy"
         
         
         echo -e "Do you want change profile proxy? [y/n]"
         read option_i2
         if [[ $option_i2 == "y" ]]; then
             #proxy for profile
-            chmod 777 '/home/keyboardkiller/profile'
-            sed -i "s@export proxy='http://${ip_proxy}@export proxy='http://${ip_proxy_change}@g" "/home/keyboardkiller/profile"
-            chmod 644 '/home/keyboardkiller/profile'
+            chmod 777 '/etc/profile'
+            sed -i "s@export proxy='http://${ip_proxy}@export proxy='http://${ip_proxy_change}@g" "/etc/profile"
+            chmod 644 '/etc/profile'
         fi
         
         echo -e "Do you want change wget proxy? [y/n]"
         read option_i3
         if [[ $option_i3 == "y" ]]; then
             #proxy for wget
-            chmod 777 '/home/keyboardkiller/wgetrc'
-            sed -i "s@https_proxy = http://${ip_proxy}/@https_proxy = http://${ip_proxy_change}/@g" "/home/keyboardkiller/wgetrc"
-            sed -i "s@http_proxy = http://${ip_proxy}/@http_proxy = http://${ip_proxy_change}/@g" "/home/keyboardkiller/wgetrc"
-            sed -i "s@ftp_proxy = http://${ip_proxy}/@ftp_proxy = http://${ip_proxy_change}/@g" "/home/keyboardkiller/wgetrc"
-            chmod 644 '/home/keyboardkiller/wgetrc'
+            chmod 777 '/etc/wgetrc'
+            sed -i "s@https_proxy = http://${ip_proxy}/@https_proxy = http://${ip_proxy_change}/@g" "/etc/wgetrc"
+            sed -i "s@http_proxy = http://${ip_proxy}/@http_proxy = http://${ip_proxy_change}/@g" "/etc/wgetrc"
+            sed -i "s@ftp_proxy = http://${ip_proxy}/@ftp_proxy = http://${ip_proxy_change}/@g" "/etc/wgetrc"
+            chmod 644 '/etc/wgetrc'
         fi
         
     fi
